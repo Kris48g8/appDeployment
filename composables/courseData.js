f94e70098a6242c4486ddd675679ba0b1995d89a
Kris48g8 <Kristoffer.sg123@gmail.com>
2023-03-08 11:14:36 +0100

    initial comit

---
+++ composables/courseData.js
@@ -0,0 +1,89 @@
+export default {
+    title: 'Test Data',
+    chapters: [
+        {
+            title: 'Chapter 1',
+            slug: 'chapter-1',
+            number: '1',
+            lessons: [
+                {
+                    title: 'Lesson 1',
+                    slug: 'lesson-1',
+                    number: '1',
+                    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil, voluptate deserunt alias odio est dolore tempore doloribus provident minus consequatur, eligendi amet quidem consequuntur. Voluptate qui neque fugit quas nam!',
+                    videoId: 1245125,
+                    sourceUrl: 'google.com',
+                    downloadUrl: 'bing.com'
+                },
+                {
+                    title: 'Lesson 2',
+                    slug: 'lesson-2',
+                    number: '2',
+                    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil, voluptate deserunt alias odio est dolore tempore doloribus provident minus consequatur, eligendi amet quidem consequuntur. Voluptate qui neque fugit quas nam!',
+                    videoId: 1245125,
+                    sourceUrl: 'google.com',
+                    downloadUrl: 'bing.com'
+                },
+                {
+                    title: 'Lesson 3',
+                    slug: 'lesson-3',
+                    number: '3',
+                    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil, voluptate deserunt alias odio est dolore tempore doloribus provident minus consequatur, eligendi amet quidem consequuntur. Voluptate qui neque fugit quas nam!',
+                    videoId: 1245125,
+                    sourceUrl: 'google.com',
+                    downloadUrl: 'bing.com'
+                }
+            ]
+        },
+        {
+            title: 'Chapter 2',
+            slug: 'chapter-2',
+            number: '2',
+            lessons: [
+                {
+                    title: 'Lesson 1',
+                    slug: 'lesson-1',
+                    number: '1',
+                    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil, voluptate deserunt alias odio est dolore tempore doloribus provident minus consequatur, eligendi amet quidem consequuntur. Voluptate qui neque fugit quas nam!',
+                    videoId: 1245125,
+                    sourceUrl: 'google.com',
+                    downloadUrl: 'bing.com'
+                },
+                {
+                    title: 'Lesson 2',
+                    slug: 'lesson-2',
+                    number: '2',
+                    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil, voluptate deserunt alias odio est dolore tempore doloribus provident minus consequatur, eligendi amet quidem consequuntur. Voluptate qui neque fugit quas nam!',
+                    videoId: 1245125,
+                    sourceUrl: 'google.com',
+                    downloadUrl: 'bing.com'
+                },
+                {
+                    title: 'Lesson 3',
+                    slug: 'lesson-3',
+                    number: '3',
+                    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil, voluptate deserunt alias odio est dolore tempore doloribus provident minus consequatur, eligendi amet quidem consequuntur. Voluptate qui neque fugit quas nam!',
+                    videoId: 1245125,
+                    sourceUrl: 'google.com',
+                    downloadUrl: 'bing.com'
+                }
+            ]
+        },
+        {
+            title: 'Chapter 3',
+            slug: 'chapter-3',
+            number: '3',
+            lessons: [
+                {
+                    title: 'Lesson 1',
+                    slug: 'lesson-1',
+                    number: '1',
+                    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil, voluptate deserunt alias odio est dolore tempore doloribus provident minus consequatur, eligendi amet quidem consequuntur. Voluptate qui neque fugit quas nam!',
+                    videoId: 1245125,
+                    sourceUrl: 'google.com',
+                    downloadUrl: 'bing.com'
+                }
+            ]
+        },
+    ]
+}
---
+++ pages/course/lesson.vue
@@ -0,0 +1,5 @@
+<template>
+    <h2>This is a lesson</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, natus similique pariatur porro repellat velit, nisi distinctio quibusdam dolorum nihil sint, hic aspernatur. Iusto qui facilis veritatis ducimus recusandae maiores!
+    </p>
+</template>
